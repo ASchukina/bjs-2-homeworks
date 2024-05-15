169086ed448521b01f433d10739059e739978224
Alexandra Schukina <alexandra.kovalenko1999@mail.ru>
2024-05-15 20:42:37 +0300

    Выполненное ДЗ, сделаны все три задания

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,100 @@
-function getArrayParams(...arr) {
+﻿'use strict';
 
+function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    else if (arr[i] < min) {
+      min = arr[i];
+    }
+      sum += arr[i];
+  }
+
+  avg = Number((sum/arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
 function summElementsWorker(...arr) {
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 }
 
 function differenceMaxMinWorker(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
 
-}
+  if(arr.length === 0) return 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    else if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+
+    return max - min;
+  }
+
 
 function differenceEvenOddWorker(...arr) {
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+
+  if(arr.length === 0) return 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if ((arr[i] % 2) === 0) {
+      sumEvenElement += arr[i];
+    }
+    else sumOddElement += arr[i];
+  }
+
+  return sumEvenElement - sumOddElement;
 
 }
 
 function averageEvenElementsWorker(...arr) {
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
 
+  if(arr.length === 0) return 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if ((arr[i] % 2) === 0) {
+      sumEvenElement += arr[i];
+      countEvenElement + 1;
+    }
+  }
+
+return sumEvenElement / countEvenElement;
 }
 
 function makeWork (arrOfArr, func) {
+  let maxWorkerResult = -Infinity;
+  let result;
+  let num;
 
+  if(arrOfArr.length === 0) return 0;
+
+  for(i = 0; i < arrOfArr.length; i++) {
+    num = arrOfArr[i];
+    result = func(...num);
+
+    if (result > maxWorkerResult) {
+      maxWorkerResult = result;
+    }
+}
+
+return maxWorkerResult;
 }
