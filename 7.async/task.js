d4812e770dcf18aad87885fb13193ca12ace26eb
Alexandra Schukina <alexandra.kovalenko1999@mail.ru>
2024-06-07 19:52:48 +0300

    Написан код с решением задачи

---
+++ 7.async/task.js
@@ -0,0 +1,62 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.intervalId = null;
+    }
+
+    addClock(time, callback) {
+        if (!time || !callback) {
+            throw new Error('Отсутствуют обязательные аргументы');
+
+        } else if (this.alarmCollection.find(alarm => alarm.time === time)) {
+            console.warn('Уже присутствует звонок на это же время');
+            return;
+
+        } else {
+            this.alarmCollection.push({
+            time,
+            callback,
+            canCall: true});
+        }
+    }
+
+
+    removeClock(time) {
+        this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
+    }
+
+    getCurrentFormattedTime() {
+        return new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+      }
+
+    start() {
+        // останавливаемся, если будильник уже запущен
+        if (this.intervalId) {
+            return;
+        }
+
+        // запускаем новый интервал
+        this.intervalId = setInterval( () => {
+            this.alarmCollection.forEach(alarm => {
+                if (alarm.time === this.getCurrentFormattedTime() && alarm.canCall) {
+                    alarm.canCall = false;
+                    alarm.callback();
+                }
+            });
+        }, 1000); // запускаем каждую секунду
+    }
+
+    stop() {
+        clearInterval(this.intervalId); // остановить интервал
+        this.intervalId = null; // сбросить значение свойства
+    }
+
+    resetAllCalls() {
+        this.alarmCollection.forEach(alarm => {alarm.canCall = true});
+    }
+    
+    clearAlarms() {
+        this.stop(); // останавливаем интервал
+        this.alarmCollection = []; // удаляем все звонки
+    }
+}
